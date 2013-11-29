1f927f88a1244d4569cec024084016d7a0f533d3
Mohammed Tangestani <tangestani@gmail.com>
2013-11-29 13:59:51 -0500

    [link.sh] fix file extension regex

--- link.sh
+++ link.sh
@@ -4,6 +4,6 @@ echo "Linking dotfiles..."
 
 cd $(dirname $0)
 
-for file in $(ls | cat | egrep -v '(.sh|.md)'); do
+for file in $(ls | cat | egrep -v '\.(sh|md)$'); do
   ln -sfv $(pwd)/${file} ~/.${file}
 done
