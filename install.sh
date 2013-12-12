b876eae6f0e1084dd18b295957114e21aa321c2c
Mohammed Tangestani <tangestani@gmail.com>
2013-12-12 04:10:47 -0500

    [install] remove some blank lines.

--- install.sh
+++ install.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Installing pathogen..."
-
 mkdir -p ~/.vim/autoload ~/.vim/bundle &&\
 curl -SsLo ~/.vim/autoload/pathogen.vim \
   https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 echo "Installing solarized..."
-
 (cd ~/.vim/bundle && git clone git://github.com/altercation/vim-colors-solarized.git)
 
 echo "Installing oh my zsh..."
-
 curl -SsL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 echo "Install iTerm2:"
